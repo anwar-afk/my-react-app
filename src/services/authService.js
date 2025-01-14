36714f4e921656adceae5fa7db452210cb0499e1
anwar <anwarfaizfauzi@gmail.com>
2025-01-14 23:44:43 +0700

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://express-production-c5e0.up.railway.app/api/auth';
+const API_URL = 'https://express-production-c596.up.railway.app/api/auth';
 
 export const register = async (userData) => {
   try {
